ca619482f2ee6c240810c14ac0cd62b1bfc8918c
Amir Hossein Soleimani <amir.hs.windows10@googlemail.com>
2023-12-07 19:22:01 +0330

    Update main.cpp

--- Session1/main.cpp
+++ Session1/main.cpp
@@ -1,13 +1,35 @@
 
+#include <cstdlib>
 #include <iostream>
 
 using namespace std;
-int main()
-{
-    int x, y, s;
-    cin >> x >> y;
-    s = x * y;
-    cout << s;
+int main() {
 
-    return 0;
+  int x, y, s;
+  cin >> x >> y;
+  s = x * y;
+  cout << "Result: " << s;
+  // back to main menu
+  string s1SelectNumberText = R"(
+    Please Select Number:
+    1.ReRUN this File
+    2.Back to Main Menu
+    )";
+  cout << s1SelectNumberText;
+  int SelectedNumber;
+  cin >> SelectedNumber;
+
+  switch (SelectedNumber) {
+  case 1: {
+    cout << system("g++ -o s1 Session1/main.cpp && ./s1");
+    cout << system("clear");
+    break;
+  }
+  case 2: {
+    cout << system("g++ -o main main.cpp && ./main");
+    cout << system("clear");
+    break;
+  }
+  }
+  return 0;
 }
