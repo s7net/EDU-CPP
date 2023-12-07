55b4981b24f1f894cd55088ab06dbb3197a8aaaf
Amir Hossein Soleimani <amir.hs.windows10@googlemail.com>
2023-12-08 03:03:07 +0330

    Update main.cpp

--- Session2/main.cpp
+++ Session2/main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
-  int a1, t1, a2, t2, a3, t3, s1, s2, s3, total;
-  string result;
+  int a1, t1, a2, t2, a3, t3, s1, s2, s3, total, result;
   const int PRICE = 200000;
-  // گرفتن اطلاعات از کاربر
+  // get data from user
   cout << "Please enter the length and width of the first room : \n sample 2 4 "
           "\n";
   cin >> a1 >> t1;
@@ -15,14 +14,15 @@ int main() {
           "\n";
   cin >> a3 >> t3;
   cout << "thanks! please wait to show result \n";
-  // محاسبه ، مقایسه و ارائه خروجی
+  // export data
   s1 = a1 * t1;
   s2 = a2 * t2;
   s3 = a3 * t3;
   total = (s1 + s2 + s3) * PRICE;
-  result = (s1 > s2 && s1 > s3)   ? "room1"
-           : (s2 > s1 && s2 > s3) ? "room2"
-                                  : "room3";
+  result =
+   (s1 > s2 && s1 > s3) ? s1 :
+   (s2 > s1 && s2 > s3) ?
+    s2 : s3;
   cout << "total price:" << total << "\n";
   cout << "more expensive room :" << result << "\n";
   return 0;
