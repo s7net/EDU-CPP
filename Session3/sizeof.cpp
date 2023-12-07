b7ddcf57e86e7168e28a05c06344d658be6c61a9
Amir Hossein Soleimani <amir.hs.windows10@googlemail.com>
2023-12-08 03:04:50 +0330

    Create sizeof.cpp

---
+++ Session3/sizeof.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    cout<<"DataType Size via sizeof function:"<<endl;
+    cout<<"short :"<< sizeof(short)<<endl;
+    cout<<"int : "<< sizeof(int)<<endl;
+    cout<<"long : "<< sizeof(long)<<endl;
+    cout<<"long int : "<< sizeof(long int)<<endl;
+    cout <<"long long int : "<< sizeof(long long int)<<endl;
+    cout<<"string : "<< sizeof(string)<<endl;
+    cout<<"float : "<< sizeof(float)<<endl;
+    cout<<"double : "<< sizeof(double)<<endl;
+    cout<<"bool : "<< sizeof(bool)<<endl;
+
+return 0;
+}
