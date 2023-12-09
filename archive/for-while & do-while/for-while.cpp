c78052302067372b3074d35e79d1f292145ab088
Amir Hossein Soleimani <amir.hs.windows10@googlemail.com>
2023-12-10 02:23:42 +0330

    Create for-while.cpp

---
+++ archive/for-while & do-while/for-while.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int number;
+  cout<<"please inter number:"<<endl;
+  cin>>number;
+    while(number<=100){
+        cout<<++number<<endl;
+    }
+return 0;
+}
