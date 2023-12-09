f11e73c844ce26784a04d8ecd0075b30d920ee4e
Amir Hossein Soleimani <amir.hs.windows10@googlemail.com>
2023-12-10 02:09:13 +0330

    Create main.cpp

---
+++ archive/Operators/main.cpp
@@ -0,0 +1,17 @@
+// برای مشاهده اموزش ویدئویی به تلگرام استاد صفایی مراجعه کنید
+// لینک : https://t.me/pcmabani/87
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=0;
+    //a+=a++;
+    //a+=a--;
+    //--a+=++a;
+    //++a+=++a;
+    //++a+=a++;
+    //--a+=a++;
+
+    cout<<"a="<<a<<endl;
+return 0;
+}
