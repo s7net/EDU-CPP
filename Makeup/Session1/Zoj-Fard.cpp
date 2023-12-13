a96f174cf1d7f55216787a08cf8f1642eb940d25
Amir Hossein Soleimani <amir.hs.windows10@googlemail.com>
2023-12-13 15:04:14 +0330

    Create Zoj-Fard.cpp

---
+++ Makeup/Session1/Zoj-Fard.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int number;
+  cin>>number;
+  if (number%2==0)
+  {
+    cout<<"zoj";
+  }
+  else 
+  {
+    cout<<"fard";
+  }
+return 0;
+}
