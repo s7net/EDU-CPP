cbe6f42f01c0f2bf844248f08f4b211e6981af69
amirhsvip <info@amirhsvip.eu.org>
2023-12-16 00:33:39 +0330

    char amal asli

---
+++ Makeup/Session1/char-amal-asli.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "please inter numbers : \n sample 2 5" << endl;
+    int number1, number2;
+    cin >> number1 >> number2;
+
+    cout << "reasult is :" << endl;
+    cout << number1 + number2 << endl;
+    cout << number1 - number2 << endl;
+    cout << number1 * number2 << endl;
+    cout << number1 / number2;
+    return 0;
+}
